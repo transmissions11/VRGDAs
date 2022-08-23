31770b11687fdab7dd9e694a4f12931c338ef992
frankie <88221686+FrankieIsLost@users.noreply.github.com>
2022-08-23 11:47:07 -0700

    fuzz

--- test/diff_fuzz/VRGDACorrectness.t.sol
+++ test/diff_fuzz/VRGDACorrectness.t.sol
@@ -5,61 +5,53 @@ import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
 
 import {LibString} from "../utils/LibString.sol";
 import {MockLinearVRGDA} from "../mocks/MockLinearVRGDA.sol";
+import {toWadUnsafe} from "../../src/utils/SignedWadMath.sol";
 import {console} from "forge-std/console.sol";
 
-
-uint256 constant TWENTY_YEARS = 356 days * 20;
-
-uint256 constant MAX_SELLABLE = 6392;
-
-int256 immutable TARGET_PRICE = 69.42e18;
-
-int256 immutable PRICE_DECREASE_PERCENT = 0.31e18;
-
-int256 immutable PER_UNIT_TIME = 0.0023e18;
-
-
+// Differentially fuzz VRGDA solidity implementation against python reference 
 contract VRGDACorrectnessTest is DSTestPlus {
     using LibString for uint256;
+
+    // sample parameters for differential fuzzing campaign 
+    uint256 immutable MAX_TIMEFRAME = 356 days * 10;
+    uint256 immutable MAX_SELLABLE = 10000;
+    int256 immutable TARGET_PRICE = 69.42e18;
+    int256 immutable PRICE_DECREASE_PERCENT = 0.31e18;
+    int256 immutable PER_UNIT_TIME = 2e18;
 
     MockLinearVRGDA vrgda;
 
     function setUp() public {
         vrgda = new MockLinearVRGDA(
-            TARGET_PRICE, // Target price.
-            PRICE_DECREASE_PERCENT, // Price decrease percent.
-            PER_UNIT_TIME // Per time unit.
+            TARGET_PRICE, 
+            PRICE_DECREASE_PERCENT,
+            PER_UNIT_TIME
         );
     }
 
-    function testFFICorrectness() public {
-    ///function testFFICorrectness(uint256 timeSinceStart, uint256 numSold) public {
-        // Limit num sold to max mint.
-        // numSold = bound(numSold, 0, MAX_MINTABLE);
-        uint256 numSold = 0;
+    function testFFICorrectness(uint256 timeSinceStart, uint256 numSold) public {
+        // Bound fuzzer inputs to acceptable contraints. 
+        numSold = bound(numSold, 0, MAX_SELLABLE);
+        timeSinceStart = bound(timeSinceStart, 0, MAX_TIMEFRAME);
+        // Convert to wad days for convenience.
+        timeSinceStart = timeSinceStart * 10e18 / 1 days;
 
-        // Limit mint time to 20 years.
-        // timeSinceStart = bound(timeSinceStart, 0, TWENTY_YEARS);
-        uint256 timeSinceStart = 1 days;
-
-        uint256 expectedPrice = calculatePrice(
+        // We wrap this call in a try catch because the getVRGDAPrice is expected to revert when 
+        // price overflows. In these cases, we continue campaign
+        try vrgda.getVRGDAPrice(int256(timeSinceStart), numSold) returns (uint256 actualPrice) {
+            uint256 expectedPrice = calculatePrice(
                 TARGET_PRICE,
                 PRICE_DECREASE_PERCENT,
                 PER_UNIT_TIME,
-                numSold,
-                timeSinceStart
-        );
+                timeSinceStart, 
+                numSold
+            );
+            assertRelApproxEq(expectedPrice, actualPrice, 0.00001e18);
 
-        console.log("AAA");
-        console.log(expectedPrice);
-
-        // uint256 actualPrice;
-
-        // console.log()
-
-
+        } catch {}
     }
 
+    // ffi call
     function calculatePrice(
         int256 _targetPrice,
         int256 _priceDecreasePercent,
@@ -82,6 +74,6 @@ contract VRGDACorrectnessTest is DSTestPlus {
         inputs[11] = "--per_time_unit";
         inputs[12] = uint256(_perUnitTime).toString();
 
-        return abi.decode(vm.ffi(inputs), (uint256));
+        return abi.decode(hevm.ffi(inputs), (uint256));
     }
 }

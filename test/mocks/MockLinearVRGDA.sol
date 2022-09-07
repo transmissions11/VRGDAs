9c2b70fea9cf6954bcc062cff5fbd50a0d6ab811
beans <certifiedbagholder@gmail.com>
2022-09-06 22:58:36 -0400

    remove comment

--- test/mocks/MockLinearVRGDA.sol
+++ test/mocks/MockLinearVRGDA.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-// import {LinearVRGDA, VRGDA} from "../../src/LinearVRGDA.sol";
 import {LinearVRGDA} from "../../src/LinearVRGDA.sol";
 
 contract MockLinearVRGDA {

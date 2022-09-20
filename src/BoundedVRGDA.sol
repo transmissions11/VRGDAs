022c7e818aa3bb937bd4f1f7aa43d6a9876c3c51
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-09-20 20:41:49 +0200

    added BoundedVRGDA, mock and test

---
+++ src/BoundedVRGDA.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import {wadExp, wadLn, wadMul, unsafeWadMul, toWadUnsafe} from "solmate/utils/SignedWadMath.sol";
+
+import {VRGDA} from "./VRGDA.sol";
+
+/// @title Bounded Variable Rate Gradual Dutch Auction
+/// @author jacopo <jacopo@slice.so>
+/// @author transmissions11 <t11s@paradigm.xyz>
+/// @author FrankieIsLost <frankie@paradigm.xyz>
+/// @notice Sell tokens roughly according to an issuance schedule.
+
+abstract contract BoundedVRGDA is VRGDA {
+    /*//////////////////////////////////////////////////////////////
+                            VRGDA PARAMETERS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev The minimum price to be paid for a token, scaled by 1e18.
+    /// @dev Represented as an 18 decimal fixed point number.
+    uint256 public immutable min;
+
+    /// @dev The maximum price to be paid for a token, scaled by 1e18.
+    /// @dev Represented as an 18 decimal fixed point number.
+    uint256 public immutable max;
+
+    /// @notice Sets pricing parameters for the VRGDA.
+    /// @param _targetPrice The target price for a token if sold on pace, scaled by 1e18.
+    /// @param _priceDecayPercent The percent price decays per unit of time with no sales, scaled by 1e18.
+    /// @param _min minimum price to be paid for a token, scaled by 1e18
+    /// @param _max maximum price to be paid for a token, scaled by 1e18
+    constructor(
+        int256 _targetPrice,
+        int256 _priceDecayPercent,
+        uint256 _min,
+        uint256 _max
+    ) VRGDA(_targetPrice, _priceDecayPercent) {
+        min = _min;
+        max = _max;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              PRICING LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Calculate the price of a token according to the VRGDA formula, bounded by min and max values.
+    /// @param timeSinceStart Time passed since the VRGDA began, scaled by 1e18.
+    /// @param sold The total number of tokens that have been sold so far.
+    /// @return The price of a token according to VRGDA, scaled by 1e18.
+    function getVRGDAPrice(int256 timeSinceStart, uint256 sold) public view virtual override returns (uint256) {
+        uint256 VRGDAPrice = super.getVRGDAPrice(timeSinceStart, sold);
+
+        return VRGDAPrice < max ? VRGDAPrice > min ? VRGDAPrice : min : max;
+    }
+}
---
+++ test/LinearBoundedVRGDA.t.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+
+import {toWadUnsafe, toDaysWadUnsafe, fromDaysWadUnsafe} from "solmate/utils/SignedWadMath.sol";
+
+import {MockLinearBoundedVRGDA} from "./mocks/MockLinearBoundedVRGDA.sol";
+
+uint256 constant ONE_THOUSAND_YEARS = 356 days * 1000;
+
+contract LinearBoundedVRGDATest is DSTestPlus {
+    MockLinearBoundedVRGDA vrgda;
+
+    function setUp() public {
+        vrgda = new MockLinearBoundedVRGDA(
+            69.42e18, // Target price.
+            0.31e18, // Price decay percent.
+            1e18, // Min price
+            100e18, // Max price
+            2e18 // Per time unit.
+        );
+    }
+
+    function testTargetPrice() public {
+        // Warp to the target sale time so that the VRGDA price equals the target price.
+        hevm.warp(block.timestamp + fromDaysWadUnsafe(vrgda.getTargetSaleTime(1e18)));
+
+        uint256 cost = vrgda.getVRGDAPrice(toDaysWadUnsafe(block.timestamp), 0);
+        assertRelApproxEq(cost, uint256(vrgda.targetPrice()), 0.00001e18);
+    }
+
+    function testPricingBasic() public {
+        // Our VRGDA targets this number of mints at the given time.
+        uint256 timeDelta = 120 days;
+        uint256 numMint = 239;
+
+        hevm.warp(block.timestamp + timeDelta);
+
+        uint256 cost = vrgda.getVRGDAPrice(toDaysWadUnsafe(block.timestamp), numMint);
+        assertRelApproxEq(cost, uint256(vrgda.targetPrice()), 0.00001e18);
+    }
+
+    function testAlwaysTargetPriceInRightConditions(uint256 sold) public {
+        sold = bound(sold, 0, type(uint128).max);
+
+        assertRelApproxEq(
+            vrgda.getVRGDAPrice(vrgda.getTargetSaleTime(toWadUnsafe(sold + 1)), sold),
+            uint256(vrgda.targetPrice()),
+            0.00001e18
+        );
+    }
+
+    function testPricingBasicMin() public {
+        uint256 timeDelta = 120 days;
+        uint256 numMint = 216;
+
+        hevm.warp(block.timestamp + timeDelta);
+
+        uint256 cost = vrgda.getVRGDAPrice(toDaysWadUnsafe(block.timestamp), numMint);
+        assertEq(cost, uint256(vrgda.min()));
+    }
+
+    function testPricingBasicMax() public {
+        uint256 timeDelta = 120 days;
+        uint256 numMint = 241;
+
+        hevm.warp(block.timestamp + timeDelta);
+
+        uint256 cost = vrgda.getVRGDAPrice(toDaysWadUnsafe(block.timestamp), numMint);
+        assertEq(cost, uint256(vrgda.max()));
+    }
+}
